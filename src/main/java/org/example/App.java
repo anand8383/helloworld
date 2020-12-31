80d96d35169a39add3a42e3514950552fbd29e63
anand8383 <56514716+anand8383@users.noreply.github.com>
2020-12-30 23:21:18 -0500

    Update App.java

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!!!!!" );
+        System.out.println( "Hello Happy New Year to everyone and God bless" );
     }
 }
